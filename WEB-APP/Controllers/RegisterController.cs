6910da8ce2ee344fbb701b3a1841318b9943abd4
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-13 14:19:45 -0300

    cadastro

---
+++ WEB-APP/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEB_APP.Models;
+
+namespace WEB_APP.Controllers {
+    public class RegisterController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(RegisterViewModel model) {
+
+            var data = new Dictionary<string, string> {
+                { "grant_type", "password" },
+                { "Password", model.Password },
+                { "Email", model.Email},
+                { "ConfirmPassword", model.ConfirmPassword },
+                { "Name", model.Nome },
+            };
+
+            using(var client = new HttpClient()) {
+                client.BaseAddress = new Uri("http://localhost:60453");
+
+                using(var requestContent = new FormUrlEncodedContent(data)) {
+
+                    var response = await client.PostAsync("Api/Account/Register", requestContent);
+                    if(response.IsSuccessStatusCode) {
+                        return RedirectToAction("Index");
+                    } else {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WEB-APP/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WEB_APP.Models {
+    public class RegisterViewModel {
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$"
+, ErrorMessage = "Informe um e-mail válido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [RegularExpression(@"^.*(?=.{6,})(?=.*[!@#$%^&+=])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$", ErrorMessage = "No minimo uma letra maiúscula, uma letra minuscula, um número e um carácter especial")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "É necessário confirmar a senha")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "As senhas não coincidem")]
+        public string ConfirmPassword { get; set; }
+    }
+}
