da3e755946d1917ddd968ce7ff889134b345db6d
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-13 14:30:22 -0300

    login

---
+++ WEB-APP/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEB_APP.Models;
+
+namespace WEB_APP.Controllers {
+    public class LoginController : Controller {
+
+        public ActionResult Login() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model) {
+            if(ModelState.IsValid) {
+                var data = new Dictionary<string, string> {
+                    { "grant_type", "password" },
+                    { "username", model.Username },
+                    { "password", model.Password }
+                };
+
+                using(var client = new HttpClient()) {
+                    client.BaseAddress = new Uri("http://localhost:60453");
+
+                    using(var requestContent = new FormUrlEncodedContent(data)) {
+                        var response = await client.PostAsync("/Token", requestContent);
+
+                        if(response.IsSuccessStatusCode) {
+                            var responseContent = await response.Content.ReadAsStringAsync();
+                            var tokenData = JObject.Parse(responseContent);
+
+                            Session.Add("access_token", tokenData["access_token"]);
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ WEB-APP/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WEB_APP.Models {
+    public class LoginViewModel {
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
